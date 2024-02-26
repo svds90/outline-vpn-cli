2b318403c9ef7e672b1ba7dbcc0c59eb6acd5cc9
Alex <svds22090@gmail.com>
2024-02-27 01:16:51 +0300

    upd

--- outline/outline_api.py
+++ outline/outline_api.py
@@ -25,7 +25,6 @@ class OutlineBase:
 class OutlineServerInfo(OutlineBase):
 
     def __init__(self, server_info: Any):
-        print('==========init OutlineServerInfo')
         self.key: str = server_info.get('server_key', "")
         self.name: str = server_info.get('name', 'Outline server')
         self.id: str = server_info.get('serverId', "")
@@ -43,7 +42,6 @@ class OutlineServerInfo(OutlineBase):
 class OutlineClientInfo(OutlineBase):
 
     def __init__(self, user_info={}):
-        print('==========init OutlineClientInfo')
         self.id: Optional[str] = user_info.get('id', None)
         self.name: str = user_info.get('name', "")
         self.password: str = user_info.get('password', "")
@@ -62,7 +60,6 @@ class OutlineServer(OutlineBase):
     """
 
     def __init__(self, outline_api_url: str):
-        print('==========init OutlineServer')
         self.outline_api_url = outline_api_url
         self.server_info = OutlineServerInfo(self.__fetch_server_info(outline_api_url))
 
@@ -158,7 +155,6 @@ class OutlineServer(OutlineBase):
 class OutlineClient(OutlineBase):
 
     def __init__(self, outline_api_url: str):
-        print('==========init OutlineClient')
         self.outline_api_url = outline_api_url
         self.client_info = OutlineClientInfo()
 
@@ -245,6 +241,5 @@ class OutlineClient(OutlineBase):
 
 class OutlineVPN(OutlineBase):
     def __init__(self, outline_api_url):
-        print('==========init OutlineVPN')
         self.server = OutlineServer(outline_api_url)
         self.client = OutlineClient(outline_api_url)
--- test.py
+++ test.py
@@ -1,8 +1,13 @@
 import click
 
 from modules.server_json_manager import JSONManager
+from outline.outline_api import OutlineVPN
 
-sm = JSONManager()
+manager = JSONManager()
+
+
+def init_outline_api(outline_api_url):
+    return OutlineVPN(outline_api_url)
 
 
 @click.group()
@@ -10,27 +15,85 @@ def cli():
     pass
 
 
-@click.command()
-@click.option('--list', '-l', 'server_id', help='Lists servers from servers.json')
-def get_info(server_id):
-    if server_id != 'all':
-        click.echo(sm.get_server(server_id))
-    else:
-        click.echo(sm.get_servers())
+@cli.command(name='get')
+@click.option('--server', '-s', 'server_id', required=True, help='Server ID')
+@click.option('--key', '-k', 'key_id', required=False, help='Key ID')
+@click.option('--keys', 'keys', is_flag=True, help='List all keys')
+@click.option('--telemetry', '-t', 'telemetry', is_flag=True, help='Get telemetry status')
+@click.option('--metrics', '-m', is_flag=True, help='Get metrics')
+def get_command(server_id, key_id, keys, telemetry, metrics):
+
+    outline = init_outline_api(manager.get_server(server_id))
+
+    if server_id and key_id and metrics:
+        click.echo(outline.client.get_all_metrics()['bytesTransferredByUserId'][str(key_id)])
+    elif server_id and keys and metrics:
+        click.echo(outline.client.get_all_metrics())
+    elif server_id and keys:
+        click.echo(outline.client.get_all_keys())
+    elif server_id and key_id:
+        outline.client.get_key(key_id)
+        click.echo(outline.client.client_info())
+    elif server_id and telemetry:
+        click.echo(outline.server.get_telemetry_status())
+    elif server_id:
+        click.echo(outline.server.server_info())
 
 
-@click.command()
-@click.option('--add', '-a', 'new_server', nargs=2, help='Adds new server to servers.json')
-def add_json(new_server):
-    if new_server:
-        idn, token = [str(i) for i in new_server]
-        sm.add_server(idn, token)
+@cli.command(name='set')
+@click.option('--server', '-s', 'server_id', required=True, help='Server ID')
+@click.option('--hostname', '-h', 'hostname', metavar='NEW HOSTNAME', help='Change hostname')
+@click.option('--name', '-n', 'name', metavar='NEW NAME', help='Change name')
+@click.option('--port', '-p', 'port', metavar='PORT', help='Change port for new keys')
+@click.option('--rename-key', 'id_name', nargs=2, metavar='ID NAME', help='Rename key')
+@click.option('--key-data-limit', 'key_limit', nargs=2, metavar='ID BYTES/"OFF"', help='Set limit per key')
+@click.option('--global-limit', 'limit', metavar='BYTES/"OFF"', help='Set global data limit')
+def set_command(server_id, hostname, name, port, id_name, key_limit, limit):
+
+    outline = init_outline_api(manager.get_server(server_id))
+
+    if server_id and name:
+        outline.server.rename_server(name)
+    elif server_id and port:
+        outline.server.change_default_port(int(port))
+    elif server_id and hostname:
+        outline.server.change_hostname(hostname)
+    elif server_id and id_name:
+        str_id, new_name = [str(i) for i in id_name]
+        outline.client.rename_key(str_id, new_name)
+    elif server_id and key_limit:
+        key_id, data_limit = key_limit
+        if data_limit == 'off':
+            outline.client.disable_data_limit(key_id)
+        else:
+            outline.client.set_data_limit(key_id, int(data_limit))
+    elif server_id and limit:
+        if limit == 'off':
+            outline.server.disable_global_data_limit()
+        else:
+            outline.server.set_global_data_limit(int(limit))
 
 
-
-def main():
-
+@cli.command(name='json')
+@click.option('--list', '-l', 'list', is_flag=True, help='Lists servers.json')
+@click.option('--get-url', '-g', 'url', metavar='ID', help='Returns server url')
+@click.option('--add', '-a', 'add', nargs=2, metavar='ID API_URL', help='Adds server')
+@click.option('--name', '-n', 'name', nargs=2, metavar='ID NEW_ID', help='Renames server')
+@click.option('--remove', '-r', 'remove', metavar='ID', help='Remove server')
+def edit_json(list, url, add, name, remove):
+    if list:
+        click.echo(manager.get_servers())
+    elif url:
+        click.echo(manager.get_server(url))
+    elif add:
+        server_name, key = add
+        manager.add_server(server_name, key)
+    elif name:
+        current_id, new_id = name
+        manager.rename_server(current_id, new_id)
+    elif remove:
+        manager.delete_server(remove)
 
 
 if __name__ == "__main__":
-    main()
+    cli()
